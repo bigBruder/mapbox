d89eb2b61e2538c700b9cd77f9615fb89d795996
romontherock <romotherock@gmail.com>
2024-05-21 10:52:12 +0300

    updated size of pins

--- src/helpers/getMarkerSizeByPoints.ts
+++ src/helpers/getMarkerSizeByPoints.ts
@@ -1,8 +1,8 @@
 export const getMarkerSizeByPoints = (points: number) => {
   switch (true) {
-    case points > 8:
-      return 35;
-    case points > 5 && points < 8:
+    case points > 10:
+      return 30;
+    case points > 7 && points < 10:
       return 25;
     default:
       return 20;
