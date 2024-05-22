069eeea4b628cae58270191c88f6ce22368a4d9f
romontherock <romotherock@gmail.com>
2024-05-22 09:32:16 +0300

    updated getting location by ip

--- src/utils/getLocationByIP.ts
+++ src/utils/getLocationByIP.ts
@@ -1,17 +1,30 @@
 import axios from "axios";
+import { Platform } from "react-native";
 import * as Network from "expo-network";
 
 export const getLocationByIP = async () => {
   try {
-    const { data } = await axios.get("https://api.ipify.org?format=json");
-    const response = await axios.get(
-      `https://freeipapi.com/api/json/${data.ip}`
-    );
-    return {
-      longitude: response.data.longitude,
-      latitude: response.data.latitude,
-      source: "ip",
-    };
+    if (Platform.OS === "ios") {
+      const { data } = await axios.get("https://api.ipify.org?format=json");
+      const response = await axios.get(
+        `https://freeipapi.com/api/json/${data.ip}`
+      );
+      return {
+        longitude: response.data.longitude,
+        latitude: response.data.latitude,
+        source: "ip",
+      };
+    }
+
+    if (Platform.OS === "android") {
+      const { data } = await axios.get("http://ip-api.com/json/");
+      console.log(data);
+      return {
+        longitude: data.lon,
+        latitude: data.lat,
+        source: "ip",
+      };
+    }
   } catch (e) {
     console.error("Error fetching location by IP:", e);
     return null;
