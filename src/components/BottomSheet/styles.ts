168ef6c666d9a81f3a0c1094c8de0feb1284b0d6
romontherock <romotherock@gmail.com>
2024-05-27 09:26:39 +0300

    increased paddings for bottomsheet

--- src/components/BottomSheet/styles.ts
+++ src/components/BottomSheet/styles.ts
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
   bottomContainer: {
     backgroundColor: "white",
     paddingTop: 20,
-    paddingBottom: 30,
+    paddingBottom: 50,
     paddingHorizontal: 30,
 
     flexDirection: "row",
