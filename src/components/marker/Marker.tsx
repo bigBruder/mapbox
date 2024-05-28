05dde44352c475195619cca8ea25017ec879309a
romontherock <romotherock@gmail.com>
2024-05-28 16:59:22 +0300

    fixed selected marker displaying

--- src/components/marker/Marker.tsx
+++ src/components/marker/Marker.tsx
@@ -64,7 +64,7 @@ export const Marker = ({
                   translateY: fadeAnim,
                 },
               ],
-              display: isImageLoading ? "none" : "flex",
+              display: isImageLoading && !isSelected ? "none" : "flex",
             }}
           >
             <ImageBackground
